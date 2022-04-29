ee397ad05b8be69fb14bd16ec8dea5ea6f8801c8
Kost Kikinov <kikinov.k@gmail.com>
2022-04-29 22:01:52 +0300

    feat: simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,28 +2,34 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  preChain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.preChain.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.preChain.push(value)
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof position === 'number' && Number.isInteger(position) && position > 0 && position < this.preChain.length) {
+      this.preChain.splice(position - 1, 1)
+      return this
+    } else {
+      this.preChain=[];
+      throw new Error ('You can\'t remove incorrect link!')
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.preChain.reverse()
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.preChain.map(elem => `( ${elem} )`).join('~~');
+    this.preChain=[];
+    return result
   }
 };
 
